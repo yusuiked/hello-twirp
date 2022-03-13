8c3b62035708660d24a238cd6a09a5942cb6ad2e
yusuiked <yukung.i@gmail.com>
2022-03-13 13:02:22 +0900

    implement client side code.

---
+++ cmd/client/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "context"
+    "net/http"
+    "os"
+    "fmt"
+    "gitlab.com/yukung/hello-twirp/rpc/haberdasher"
+)
+
+func main() {
+    client := haberdasher.NewHaberdasherProtobufClient("http://localhost:8080", &http.Client{})
+
+    hat, err := client.MakeHat(context.Background(), &haberdasher.Size{Inches: 12})
+    if err != nil {
+        fmt.Printf("oh no: %v", err)
+        os.Exit(1)
+    }
+    fmt.Printf("I have a nice new hat: %+v", hat)
+}
